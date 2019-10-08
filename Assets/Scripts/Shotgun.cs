4041a038c743d7d01ca82066c272ded19dd6b083
Sophia Pineda <sophia.pineda@hotmail.com>
2019-10-08 04:19:29 -0400

    Pistol script added to GameObj

---
+++ Assets/Scripts/Shotgun.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pistol : Gun
+{
+    protected override void update()
+    {
+        base.update();
+        /*Shotgun & pistol have semi-auto fire rate*/
+        if (Input.GetMouseButtonDown(0) && (Time.time - lastFireTime)
+            > fireRate )
+        {
+            lastFireTime = Time.time;
+            Fire();
+        }
+    }
+}
