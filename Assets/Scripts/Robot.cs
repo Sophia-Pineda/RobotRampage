edd164baf5acb976adf6bd4e9b19cda4347425e9
Sophia Pineda <sophia.pineda@hotmail.com>
2019-10-12 16:39:50 -0400

    Robot Script & prefabs

---
+++ Assets/Scripts/Robot.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Robot : MonoBehaviour
+{
+    [SerializeField]
+    private string robotType;
+
+    public int health;
+    public int range;
+    public float fireRate;
+    public Transform missileFireSpot;
+    UnityEngine.AI.NavMeshAgent agent;
+    private Transform player;
+    private float timeLastFired;
+    private bool isDead;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
