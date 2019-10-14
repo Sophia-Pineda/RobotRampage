f5fa5b6b9493f20168c0bd8fb28b06b8b75bebbb
Sophia Pineda <sophia.pineda@hotmail.com>
2019-10-13 23:03:59 -0400

    update Robot script. Bots fire missiles.

--- Assets/Scripts/Robot.cs
+++ Assets/Scripts/Robot.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class Robot : MonoBehaviour
 {
+
     [SerializeField]
     private string robotType;
+    [SerializeField]
+    GameObject missileprefab;
 
     public int health;
     public int range;
@@ -53,6 +56,9 @@ public class Robot : MonoBehaviour
 
   private void fire()
          {
+        GameObject missile = Instantiate(missileprefab);
+        missile.transform.position = missileFireSpot.transform.position;
+        missile.transform.rotation = missileFireSpot.transform.rotation;
         robot.Play("Fire");
          }
    
