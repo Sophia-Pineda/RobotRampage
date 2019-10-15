b71623dcbf9ceb984fbe5064e4072368daffa246
Sophia Pineda <sophia.pineda@hotmail.com>
2019-10-15 18:41:33 -0400

    PickupSpawn script

---
+++ Assets/Scripts/PickupSpawn.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupSpawn : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] pickups;
+
+    //1
+    void spawnPickup()
+    {
+        //Instantiate random pickup
+
+        GameObject pickup = Instantiate(pickups[Random.Range(0, pickups.Length)]);
+        pickup.transform.position = transform.position;
+        pickup.transform.parent = transform;
+
+    }
+    //2 
+    IEnumerator respawnPickup()
+    {
+        yield return new WaitForSeconds(20);
+        spawnPickup();
+
+    }
+    //3
+    private void Start()
+    {
+        spawnPickup();
+
+    }
+    //4
+    public void PickupWasPickedUp()
+    {
+        StartCoroutine("respawnPickup");
+    }
+}
